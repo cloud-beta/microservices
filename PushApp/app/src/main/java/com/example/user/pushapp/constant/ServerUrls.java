b703413561aae418e7abc67941201e7256aa6e77
jjy <galid1@naver.com>
2019-01-28 17:37:03 +0900

    Android App - constant/ServerUrls URL 변경 - MyFirebaseMessagingService 푸시알람 클릭시 넘겨 받은 URL로 이동하기

--- PushApp/app/src/main/java/com/example/user/pushapp/constant/ServerUrls.java
+++ PushApp/app/src/main/java/com/example/user/pushapp/constant/ServerUrls.java
@@ -1,5 +1,5 @@
 package com.example.user.pushapp.constant;
 
 public class ServerUrls {
-    public static final String PUSHSERVER_URL = "http://210.108.48.230:8080";
+    public static final String PUSHSERVER_URL = "http://210.108.48.230:8080/token";
 }
